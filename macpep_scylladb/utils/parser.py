1b1330eb6e4367f6b4bb0ba7ee855ea5db9163a1
kulgg <75735874+kulgg@users.noreply.github.com>
2022-10-24 21:40:40 +0200

    Add is reviewed parsing

--- macpep_scylladb/utils/parser.py
+++ macpep_scylladb/utils/parser.py
@@ -6,6 +6,7 @@ ACCESSION_LINES_REGEX = re.compile("AC   .*", re.MULTILINE)
 ACCESSIONS_REGEX = re.compile("(\w+);\s?")
 SEQUENCE_LINES_REGEX = re.compile("^     \w+.*", re.MULTILINE)
 SEQUENCE_REGEX = re.compile("(\w+)\s?")
+REVIEWED_STATUS_REGEX = re.compile("^ID.*?Reviewed;", re.MULTILINE)
 
 def get_id(txt: str) -> str:
     match = ID_REGEX.match(txt)
@@ -24,3 +25,6 @@ def get_sequence(txt: str) -> str:
         for sequence_part in SEQUENCE_REGEX.findall(line):
             sequence = f"{sequence}{sequence_part}"
     return sequence
+
+def get_review_status(txt: str) -> bool:
+    return bool(REVIEWED_STATUS_REGEX.search(txt))
