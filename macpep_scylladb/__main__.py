f3ead4b2bce562e3679cca29c8844337a13ce168
kulgg <75735874+kulgg@users.noreply.github.com>
2022-10-24 15:32:28 +0200

    Add CLI

---
+++ macpep_scylladb/__main__.py
@@ -0,0 +1,28 @@
+import logging
+import sys
+
+import fire
+from exitstatus import ExitStatus
+
+from macpep_scylladb.modules.UniprotDigest import UniprotDigest
+
+
+class Commands():
+    def __init__(self):
+        self.digest = UniprotDigest()
+
+def configure_logging():
+    log_format = "%(asctime)s [%(levelname)s] %(module)s.%(funcName)s(): %(message)s"
+    logging.basicConfig(level=logging.INFO, format=log_format)
+
+def main():
+    configure_logging()
+
+    try:
+        fire.Fire(Commands)
+    except Exception as exception:
+        logging.exception(exception)
+        sys.exit(ExitStatus.failure)
+
+if __name__ == "__main__":
+    main()
---
+++ macpep_scylladb/modules/UniprotDigest.py
@@ -0,0 +1,6 @@
+class UniprotDigest:
+    def __init__(self):
+        self.ipfs_url = "http://ipfs.io/ipfs/"
+
+    def do(self, blah: str):
+        print(blah)
