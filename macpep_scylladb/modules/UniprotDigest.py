87ae7aea291a58509d89f7164bbfe694819a06d9
kulgg <75735874+kulgg@users.noreply.github.com>
2022-10-24 16:24:14 +0200

    Add pycache to gitignore

--- macpep_scylladb/modules/UniprotDigest.py
+++ macpep_scylladb/modules/UniprotDigest.py
@@ -1,6 +1,29 @@
+from io import TextIOWrapper
+
+
 class UniprotDigest:
     def __init__(self):
-        self.ipfs_url = "http://ipfs.io/ipfs/"
+        pass
 
-    def do(self, blah: str):
-        print(blah)
+    def digest(self, path: str):
+        f = open(path, "r")
+        a = self.get_next_protein(f)
+        print(a)
+        f.close()
+        
+    def get_next_protein(self, f: TextIOWrapper) -> str:
+        protein = ""
+        line = f.readline()
+
+        while line != "//\n":
+            protein += line
+            line = f.readline()
+        
+        return protein
+
+
+"""
+Get all // separated proteins
+Get all protein info fields
+Digest Sequence
+"""
