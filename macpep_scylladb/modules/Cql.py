362d4a83c1f9056d5c5a880677214a08096ceff8
kulgg <75735874+kulgg@users.noreply.github.com>
2023-01-03 17:18:14 +0100

    Add peptide upsert

--- macpep_scylladb/modules/Cql.py
+++ macpep_scylladb/modules/Cql.py
@@ -1,3 +1,4 @@
+import logging
 import random
 import string
 from cassandra.cqlengine.management import sync_table
@@ -40,6 +41,49 @@ class Cql:
         sync_table(Peptide)
         sync_table(Protein)
 
+    def upsert_peptide(self, server: str, p: Peptide):
+        connection.setup([server], "macpep")
+        p.validate()
+        attrs = p._as_dict()
+        # This seems to do it in one command, instead of SELECT + Create/Update
+        Peptide.objects(
+            partition=attrs["partition"],
+            mass=attrs["mass"],
+            sequence=attrs["sequence"],
+        ).update(
+            proteins__add=attrs["proteins"],
+            length=attrs["length"],
+            number_of_missed_cleavages=attrs["number_of_missed_cleavages"],
+            a_count=attrs["a_count"],
+            b_count=attrs["b_count"],
+            c_count=attrs["c_count"],
+            d_count=attrs["d_count"],
+            e_count=attrs["e_count"],
+            f_count=attrs["f_count"],
+            g_count=attrs["g_count"],
+            h_count=attrs["h_count"],
+            i_count=attrs["i_count"],
+            j_count=attrs["j_count"],
+            k_count=attrs["k_count"],
+            l_count=attrs["l_count"],
+            m_count=attrs["m_count"],
+            n_count=attrs["n_count"],
+            o_count=attrs["o_count"],
+            p_count=attrs["p_count"],
+            q_count=attrs["q_count"],
+            r_count=attrs["r_count"],
+            s_count=attrs["s_count"],
+            t_count=attrs["t_count"],
+            u_count=attrs["u_count"],
+            v_count=attrs["v_count"],
+            w_count=attrs["w_count"],
+            y_count=attrs["y_count"],
+            z_count=attrs["z_count"],
+            n_terminus=attrs["n_terminus"],
+            c_terminus=attrs["c_terminus"],
+            is_metadata_up_to_date=attrs["is_metadata_up_to_date"],
+        )
+
     def insert_loop(self, server: str, elements: int):
         connection.setup([server], "macpep")
         for _ in range(elements):
@@ -52,54 +96,64 @@ class Cql:
 
     def insert_test(self, server: str):
         connection.setup([server], "macpep")
-        Peptide(
-            partition=0, mass=502286345739, sequence="QLSR", proteins={protein}
-        ).save()
-        Peptide(
-            partition=2, mass=945474040024, sequence="SQRDRER", proteins={protein}
-        ).save()
-        Peptide(
-            partition=8,
-            mass=3684549317922,
-            sequence="ERLSDSSAPSSLGTGYFCDSDSDQEEKASDASSEK",
-            proteins={protein},
-        ).save()
-        Peptide(
-            partition=5,
-            mass=2529289540729,
-            sequence="AGLGTGAAGGIGAGRTRAPSLASSSGSDK",
-            proteins={protein},
-        ).save()
-        Peptide(
-            partition=6,
-            mass=2947478329205,
-            sequence="ISGLERSQEKSQDCCKEPVFEPVVLK",
-            proteins={protein},
-        ).save()
-        Peptide(
-            partition=7,
-            mass=3018410767455,
-            sequence="DREPHDYSHHHHHHHHPLAVDPRR",
-            proteins={protein},
-        ).save()
-        Peptide(
-            partition=9,
-            mass=3841096254963,
-            sequence="ISPTAGHQNGLLNKTPPTAALSAPPPLISTLGGRPGSPR",
-            proteins={protein},
-        ).save()
-        Peptide(
-            partition=4,
-            mass=1614738647319,
-            sequence="EDHDLPTEAPQAHR",
-            proteins={protein},
-        ).save()
-        Peptide(
-            partition=3,
-            mass=1612755360019,
-            sequence="SASAAAHDRDRDVDK",
-            proteins={protein},
-        ).save()
-        Peptide(
-            partition=1, mass=589307140804, sequence="SSTPAK", proteins={protein}
-        ).save()
+        self.upsert_peptide(
+            server,
+            Peptide(
+                partition=0,
+                mass=502286345739,
+                sequence="QLSR",
+                proteins={protein},
+                length=4,
+                number_of_missed_cleavages=0,
+                n_terminus=0,
+                c_terminus=0,
+            ),
+        )
+        # Peptide(
+        #     partition=2, mass=945474040024, sequence="SQRDRER", proteins={protein}
+        # ).save()
+        # Peptide(
+        #     partition=8,
+        #     mass=3684549317922,
+        #     sequence="ERLSDSSAPSSLGTGYFCDSDSDQEEKASDASSEK",
+        #     proteins={protein},
+        # ).save()
+        # Peptide(
+        #     partition=5,
+        #     mass=2529289540729,
+        #     sequence="AGLGTGAAGGIGAGRTRAPSLASSSGSDK",
+        #     proteins={protein},
+        # ).save()
+        # Peptide(
+        #     partition=6,
+        #     mass=2947478329205,
+        #     sequence="ISGLERSQEKSQDCCKEPVFEPVVLK",
+        #     proteins={protein},
+        # ).save()
+        # Peptide(
+        #     partition=7,
+        #     mass=3018410767455,
+        #     sequence="DREPHDYSHHHHHHHHPLAVDPRR",
+        #     proteins={protein},
+        # ).save()
+        # Peptide(
+        #     partition=9,
+        #     mass=3841096254963,
+        #     sequence="ISPTAGHQNGLLNKTPPTAALSAPPPLISTLGGRPGSPR",
+        #     proteins={protein},
+        # ).save()
+        # Peptide(
+        #     partition=4,
+        #     mass=1614738647319,
+        #     sequence="EDHDLPTEAPQAHR",
+        #     proteins={protein},
+        # ).save()
+        # Peptide(
+        #     partition=3,
+        #     mass=1612755360019,
+        #     sequence="SASAAAHDRDRDVDK",
+        #     proteins={protein},
+        # ).save()
+        # Peptide(
+        #     partition=1, mass=589307140804, sequence="SSTPAK", proteins={protein}
+        # ).save()
