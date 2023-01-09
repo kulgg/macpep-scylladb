d3b4151cbb3b053752bbd2c1d6541f2b82c11845
kulgg <75735874+kulgg@users.noreply.github.com>
2023-01-09 16:48:37 +0100

    Cleanup Inserter

--- macpep_scylladb/modules/Inserter.py
+++ macpep_scylladb/modules/Inserter.py
@@ -23,10 +23,7 @@ class Inserter:
         self.cql = cql
         self.stopped = False
 
-    def _process_peptides(self, protein: Protein, cql=None, session: Session = None):
-        if not cql:
-            cql = self.cql
-
+    def _process_peptides(self, protein: Protein, session: Session = None):
         peptides = []
         for peptide_sequence, num_of_missed_cleavages in self.proteomics.digest(
             protein.sequence
@@ -77,31 +74,25 @@ class Inserter:
                 proteome_ids={protein.proteome_id},
             )
             if not session:
-                cql.upsert_peptide(self.server, peptide)
+                self.cql.upsert_peptide(self.server, peptide)
+                self.num_peptides += 1
             else:
                 peptides.append(peptide)
         if session:
-            cql.upsert_peptides(session, peptides)
+            self.cql.upsert_peptides(session, peptides)
             with self.lock:
                 self.num_peptides += len(peptides)
 
-    def _worker(self, queue, use_concurrency):
-        session = None
-        cluster = None
-        cql = None
-        if use_concurrency:
-            cluster = Cluster([self.server])
-            session = cluster.connect("macpep")
-        else:
-            cql = Cql()
+    def _worker(self, queue):
+        cluster = Cluster([self.server])
+        session = cluster.connect("macpep")
 
         while True:
             protein = queue.get()
-            if protein is None:
-                if use_concurrency:
-                    cluster.shutdown()
+            if not protein:
+                cluster.shutdown()
                 break
-            self._process_peptides(protein, cql, session)
+            self._process_peptides(protein, session)
 
     def _progress_worker(self, queue):
         old_num_processed = 0
@@ -131,24 +122,24 @@ class Inserter:
         uniprot_f = open(uniprot_file_path, "r")
         reader = UniprotTextReader(uniprot_f)
 
-        num_proteins = 0
-        num_peptides = 0
+        self.num_proteins = 0
+        self.num_peptides = 0
 
         start_time = time.time()
         for protein in reader:
-            num_proteins += 1
+            self.num_proteins += 1
             protein_db = to_database(protein)
             self.cql.insert_protein(server, protein_db)
             self._process_peptides(protein)
             elapsed_time = time.time() - start_time
-            items_per_second = num_proteins / elapsed_time
+            items_per_second = self.num_proteins / elapsed_time
             bar.suffix = (
-                f"{num_proteins}/{num_lines} {items_per_second:.2f} proteins/sec"
+                f"{self.num_proteins}/{num_lines} {items_per_second:.2f} proteins/sec"
             )
             bar.next()
 
-        logging.info("Number of proteins: %d", num_proteins)
-        logging.info("Number of peptides: %d", num_peptides)
+        logging.info("Number of proteins: %d", self.num_proteins)
+        logging.info("Number of peptides: %d", self.num_peptides)
 
         uniprot_f.close()
 
@@ -157,7 +148,6 @@ class Inserter:
         server: str,
         partitions_file_path: str,
         uniprot_file_path: str,
-        use_concurrency=True,
         num_threads: int = 14,
     ):
         self.server = server
@@ -181,7 +171,7 @@ class Inserter:
         self.lock = threading.Lock()
         threads = []
         for _ in range(num_worker_threads):
-            t = threading.Thread(target=self._worker, args=(queue, use_concurrency))
+            t = threading.Thread(target=self._worker, args=(queue,))
             t.start()
             threads.append(t)
 
@@ -196,7 +186,7 @@ class Inserter:
             self.cql.insert_protein(self.server, to_database(protein))
             queue.put(protein)
 
-        for _ in range(num_worker_threads):
+        for _ in range(num_worker_threads + 1):
             queue.put(None)
 
         for thread in threads:
