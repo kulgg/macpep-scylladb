93a3e794a11adbbc5a363973da528c5c03c63779
kulgg <75735874+kulgg@users.noreply.github.com>
2023-02-03 17:02:45 +0100

    .

--- macpep_scylladb/utils/queries.py
+++ macpep_scylladb/utils/queries.py
@@ -9,10 +9,15 @@ def query_peptides_with_callback(
     result_cb,
     error_cb,
 ):
-    query_str = """SELECT * FROM macpep.peptides WHERE "partition" = ? AND "mass" >= ? AND "mass" <= ?"""
-    query_statement = session.prepare(query_str)
+    query_str = (
+        'SELECT * FROM macpep.peptides WHERE "partition" = {partition} AND "mass" >='
+        ' {lower} AND "mass" <= {upper}'
+    )
+    # query_statement = session.prepare(query_str)
 
-    params = (partition, lower_mass, upper_mass)
-    future = session.execute_async(query_statement, params)
+    # params = (partition, lower_mass, upper_mass)
+    future = session.execute_async(
+        query_str.format(partition=partition, lower=lower_mass, upper=upper_mass)
+    )
 
     future.add_callbacks(result_cb, error_cb)
